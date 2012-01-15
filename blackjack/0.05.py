f43ba8a16b4a01f3438c39ded1cb255fbfb04483
Justin Nearing <mistanearing@gmail.com>
2012-01-14 22:15:35 -0800

    upload version 0.05.py

--- blackjack/0.05.py
+++ blackjack/0.05.py
@@ -2,7 +2,7 @@
 
 #CHANGELIST:
 #0.02 - 13/01/12 23:17-0:47
-#0.03 - 14/01/12 12:50-14:20; 15:00-17:35
+#0.03 - 14/01/12 12:50-14:20; 15:00-17:35; 19:30-22:10
 
 #NOTES:
 #V3, major refactor, gutted crappy code as I better understand how I want the system to work
@@ -53,34 +53,49 @@ class Deck(object):
 
 
 class Deal(object):
-    def __init__(self, deck):
+    def __init__(self, deck, deck_dictionary):
         self.deck = deck
-        self.card_and_deck = []
-        self.players_hand = []
-        self.dealers_hand = []
+        self.card_and_deck = ['null']
+        self.hand = []
+        self.dealers_card = []
+        self.card = "null"
 
-    def deal(self):
-        get_first_card = self.get_card_and_deck(self.deck)
-        first_card = self.card_or_deck(get_first_card, 'card')
+    #What's supposed to happen here is you getting 2 player cards and one dealer card. 
+    #For some reason, dealer_card only is returning get_card_and_deck
+    def get_hand(self):
+        get_first_card = self.get_card_and_deck(self.card, self.deck)
         deck = self.card_or_deck(get_first_card, 'deck')
-
-        get_dealer_card = self.get_card_and_deck(deck)
-        dealer_card = self.card_or_deck(get_dealer_card, 'card')
+        first_card = self.card_or_deck(get_first_card, 'card')
+        print "this is first_card\n"
+        
+        get_dealer_card = self.get_card_and_deck(first_card, deck)
         deck = self.card_or_deck(get_dealer_card, 'deck')
-                
-        print "Player is dealt: ", first_card
-        print "Dealer is dealt: ", dealer_card
+        dealer_card = self.card_or_deck(get_dealer_card, 'card')
+        print "This is dealer_card\n", dealer_card
 
+        get_second_card = self.get_card_and_deck(dealer_card, deck)        
+        deck = self.card_or_deck(get_second_card, 'deck')
+        second_card = self.card_or_deck(get_second_card, 'card')
+        print "This is second_card\n"
 
-    def get_card_and_deck(self, deck):
-        self.deck = deck
+        hand = [first_card, second_card, dealer_card]
+        return hand
+        
+
+    def get_card_and_deck(self, card, deck):
+        deck = self.deck
+        card = self.card
         card = deck.pop(randint(0, len(deck)-1))
+
+        del self.card_and_deck[0]
         self.card_and_deck.append(card)
         self.card_and_deck.append(deck)
         return self.card_and_deck
         
     def card_or_deck(self, card_and_deck, switch):
+        #print self.card_and_deck[0]
         self.card_and_deck = card_and_deck
+        #print self.card_and_deck[0]
         if switch == 'card':
             return self.card_and_deck[0]
         elif switch == 'deck':
@@ -88,22 +103,38 @@ class Deal(object):
         else:
             return 'Class: Deal - Func: split - fatal error' 
 
-    
+    def deal(self, hand, deck, deck_dictionary):
+        self.deck_dictionary = deck_dictionary
+        self.hand = hand
+        self.deck = deck
+        print "card one: ", self.hand[0]
+        print "card two ", self.hand[1]
+#        print "card dealer ", self.hand[2]
+#        first_card_value = self.deck_dictionary[self.hand[0]]
+#        second_card_value = self.deck_dictionary[self.hand[1]]
+#        dealer_card_value = self.deck_dictionary[self.hand[2]]
+#        print "Player is dealt: ", self.hand[0]
+#        print "Dealer is dealt: ", self.hand[2]
+#        print "Player is dealt: ", self.hand[1]
+#        print "\nPlayers total: ", (first_card_value + second_card_value)   
+#        print "\n\n\n", len(deck)   
+
         
 
 #-----------------RUNTIME--------------------------------------------------------------------
 
 
 
-print "BLACKJACK"
+print "\n\nWELCOME TO BLACKJACK\n\n"
 
 create_deck = Deck()
 deck = create_deck.create_deck()
 deck_dictionary = create_deck.create_deck_dictionary()
 
 
-card = Deal(deck)
-deal = card.deal()
+card = Deal(deck, deck_dictionary)
+get_hand = card.get_hand()
+deal = card.deal(get_hand, deck, deck_dictionary)
 
 
 
